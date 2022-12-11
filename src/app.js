7f033246fa45d5aba75e33a4bfc27250ce361cd1
Juliuuss <j.esp.ver@gmail.com>
2022-12-11 23:31:16 +0000

    final

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dom = [".com", ".cl"];
+let domainName = [];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let index = 0; index < pronoun.length; index++) {
+    for (let index2 = 0; index2 < adj.length; index2++) {
+      for (let index3 = 0; index3 < noun.length; index3++) {
+        for (let index4 = 0; index4 < dom.length; index4++) {
+          console.log(
+            pronoun[index] + adj[index2] + noun[index3] + dom[index4]
+          );
+          domainName.push(
+            pronoun[index] + adj[index2] + noun[index3] + dom[index4]
+          );
+        }
+      }
+    }
+  }
+  console.log(domainName);
 };
